817b264d8fc88430009e0ed3b2c4a32e60101536
Domenico Villani <domenico.villani.99@gmail.com>
2023-11-14 16:55:26 +0100

    funzione js page 4 commentata domenico

--- assets/js/pag_4_feedback.js
+++ assets/js/pag_4_feedback.js
@@ -1,24 +1,26 @@
-let numStar = 10
-const arrayImg = []
+let numStar = 10                                                    //variabile per il numero delle stelle
+const arrayImg = []                                                 //array per le immagini inserite 
 
 
-const deselectorStar = function(){
-    const selected = document.querySelectorAll(".starSelected")
+const deselectorStar = function(){                                  //funzione che invoco dopo per deselezionare le stelle
+    const selected = document.querySelectorAll(".starSelected")     //seleziono tutte le stelle con classe star selected
     for(let i=0; i<selected.length; i++){
-        if (selected) selected.classList.add("starNotSelected") 
+        if (selected) {
+            selected.classList.add("starNotSelected")               //aggiungo alle classi la classe non selected
+        }     
     }
 }
 
-const selectStar = function(indexStar){
+const selectStar = function(indexStar){                             //funzione che invoco dopo per selezionare le stelle
     let i = 0
-    arrayImg[i].classList.add("starSelected")
-    while(arrayImg[i] !== indexStar){
+    arrayImg[i].classList.add("starSelected")                       //aggiungo la classe selected alla prima stella
+    while(arrayImg[i] !== indexStar){                               //aggiungo la classe finche' non arrivo al numero cliccato
         i++
-        arrayImg[i].classList.add("starSelected")
+        arrayImg[i].classList.add("starSelected")                   
     }
 }
 
-const createStar = function(){
+const createStar = function(){                                      //funzione che crea tutte le immagini delle stelle 
     const contRate = document.querySelector("#rateStar")
     for(let i=0; i<numStar; i++){
         const contStar = document.createElement("div")
@@ -27,10 +29,10 @@ const createStar = function(){
         imgStar.classList.add("starNotSelected")
         contRate.appendChild(imgStar)
         arrayImg[i] = imgStar
-        imgStar.addEventListener("click", function (e){
+        imgStar.addEventListener("click", function (e){             //al click invoco funzioni per
             e.preventDefault()
-            deselectorStar()
-            selectStar(imgStar)
+            deselectorStar()                                        //deselezionare tutte le stelle
+            selectStar(imgStar)                                     //selezionare un numero di stelle definito
         })
     }
 
