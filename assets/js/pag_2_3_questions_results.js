466087af8eea138a5064b17a471adfb60f1f40a1
unknown <meethabeelkinde@gmail.com>
2023-11-14 15:57:08 +0100

    js pag 3 e modifiche nel HTML pg3

--- assets/js/pag_2_3_questions_results.js
+++ assets/js/pag_2_3_questions_results.js
@@ -7,3 +7,14 @@ const calcoloPercentuale = (risposte) => {
         wrong
     }
 }
+
+const mexNelCerchio = (risposte) => {
+    if(risposte > 5){
+        const messaggio = document.querySelector('#circle h4')
+        messaggio.innerHTML = 'Congratulation! <span>You passed the exam.<span>'
+    }
+    else{
+        const messaggio = document.querySelector('#circle h4')
+        messaggio.innerHTML = "Test Fail <span>You didn't passed the exam.<span>"
+    }
+}
