aab4bc33866738494ba5a2e4bc7c0cb16ed1d18f
matteofalco1986 <matteo.falco1986@gmail.com>
2023-11-14 14:56:16 +0000

    HTML tweaks and minor CSS upgrades

--- assets/js/pag_4_feedback.js
+++ assets/js/pag_4_feedback.js
@@ -1,17 +1,39 @@
 let numStar = 10
+const arrayImg = []
+
+
+const deselectorStar = function(){
+    const selected = document.querySelectorAll(".starSelected")
+    for(let i=0; i<selected.length; i++){
+        if (selected) selected.classList.add("starNotSelected") 
+    }
+}
+
+const selectStar = function(indexStar){
+    let i = 0
+    arrayImg[i].classList.add("starSelected")
+    while(arrayImg[i] !== indexStar){
+        i++
+        arrayImg[i].classList.add("starSelected")
+    }
+}
 
 const createStar = function(){
     const contRate = document.querySelector("#rateStar")
     for(let i=0; i<numStar; i++){
         const contStar = document.createElement("div")
-        let img = document.createElement("img")
-        img.src = 'assets/images/star.svg'
-        contRate.appendChild(img)
-        contStar.addEventListener("click", function (e){
+        let imgStar = document.createElement("img")
+        imgStar.src = 'assets/images/star.svg'
+        imgStar.classList.add("starNotSelected")
+        contRate.appendChild(imgStar)
+        arrayImg[i] = imgStar
+        imgStar.addEventListener("click", function (e){
             e.preventDefault()
-            
+            deselectorStar()
+            selectStar(imgStar)
         })
     }
+
 }
 
 createStar()
