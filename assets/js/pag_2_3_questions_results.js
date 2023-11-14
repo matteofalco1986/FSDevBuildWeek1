568a167ec49a11c77abe5deb718f3a22163126d6
unknown <meethabeelkinde@gmail.com>
2023-11-14 16:54:14 +0100

    Aggiunta commenti piu la classe hidePage

--- assets/js/pag_2_3_questions_results.js
+++ assets/js/pag_2_3_questions_results.js
@@ -1,6 +1,6 @@
 const calcoloPercentuale = (risposte) => {
-    const correct = risposte/10
-    const wrong = (10-risposte)/10
+    const correct = risposte/10 /* percentuale risposte corrette */
+    const wrong = (10-risposte)/10 /* percentuale risposte sbagliate */
 
     return {
         correct,
@@ -9,11 +9,11 @@ const calcoloPercentuale = (risposte) => {
 }
 
 const mexNelCerchio = (risposte) => {
-    if(risposte > 5){
+    if(risposte > 5){ /* se la soglia è maggiore a la metà delle domande allora l'esame è superato */
         const messaggio = document.querySelector('#circle h4')
         messaggio.innerHTML = 'Congratulation! <span>You passed the exam.<span>'
     }
-    else{
+    else{ /* nel caso contrario il messagio sarà che l'esame non è stato superato */
         const messaggio = document.querySelector('#circle h4')
         messaggio.innerHTML = "Test Fail <span>You didn't passed the exam.<span>"
     }
