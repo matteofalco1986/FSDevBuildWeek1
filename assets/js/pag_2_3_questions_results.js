637a636c1accf072c699203201a28e1dfff87ff8
Massimo Emanuele Spinosa <massimospinosa72@gmail.com>
2023-11-15 11:12:48 +0100

    parte di js mercoledi mattina

--- assets/js/pag_2_3_questions_results.js
+++ assets/js/pag_2_3_questions_results.js
@@ -123,62 +123,136 @@ const questions = [
       incorrect_answers: ["Python", "C", "Jakarta"],
     },
   ];
-  
- //funzione timer countdown 60 secondi
-  const timer = function(indexCount){ /* aggiunta di un argomento nella funzione timer per poter risettare facilmente il count */
-    let time 
-    let end
-    let count = indexCount
-    let containerTimer = document.querySelector('#timer span')
-    containerTimer.innerHTML = count
-    time = setTimeout(update,1000)
-    function update(){
-        if(count>0){
-            containerTimer.innerHTML = --count
-            time = setTimeout(update,1000)            
-        }else{
-            end = "fine"
-        }
-    }
-    return count /* importante aggiungere il return per tracciare il count */
-}
+
+/*RAMONA
+/*   timer()  *//* ho richiamato la funzione timer per controllare il suo andamento sulla pagina*/
+showQuestion() //richiamo la funzione per controllare il suo andamento
+
+let index = 0 //inizializzo indice che gestirà array questions
+ let contIncorrect = 0 // inizializzo contatore domande errate verrà utilizzato nella funzione checkAnswer/timer
+ let contCorrect = 0 // inizializzo contatore domande corrette
+ let answerUser = ''
+
 
 //funzione che deseleziona 
   const deselezionaClasse = function (){
-    const selezionati = document.querySelector(".isSelected");
-    if (selezionati){
-        selezionati.classList.remove("isSelected");
-    } 
+    const selezionati = document.querySelector(".select");
+    if (selezionati) selezionati.classList.remove("select");
   } 
   
 //funzione crea domande 
-  const showQuestion = function(){
-    
-    for(let i=0; i < questions.length; i++){ // ogni domanda
-        let count = timer(60)
-        const quest = document.getElementById("quest")  
-        const answer = document.getElementsByClassName("answer")
-        quest.innerHTML = questions[i].question; //inserisco testo domanda
-        answer[0].innerHTML = questions[i].correct_answer // fino a riga 141 inserisco testo risposte in ogni contenitore con classe answer
-        answer[1].innerHTML = questions[i].incorrect_answers[0]
-        answer[2].innerHTML = questions[i].incorrect_answers[1]
-        answer[3].innerHTML = questions[i].incorrect_answers[2]
-        const countQuest = document.querySelector('#countQuestion span') 
-        countQuest.innerHTML = i+1 // nel paragrafo a piepagina inserisco l'indice dell'array di oggetti tiene il conto di ogni domanda 
-        for (let i=0; i < answer.length; i++){  // ad ogni bottone assegno la classe di selezione e la rimuovo a gli altri elementi 
-            answer.addEventListener("click", function(e){
-                deselezionaClasse()
-                answer.classList.add("isSelected");
-            })
-        }
-        if(count>0){ 
-            //l'utente ha ancora tempo di rispondere
-        }
-        else{
-            //passiamo alla prossima domanda
-        }               
+  const showQuestion = function(i){// ogni domanda passo come parametro l'index dichiarato e inizializzato alla riga 108 // dovrà essere incrementato al passare dei 60 secondi o al click del button nextQuestion
+      const quest = document.getElementById("quest")  
+      const answers = document.getElementsByClassName("answer")
+      quest.innerHTML = questions[i].question; //inserisco testo domanda
+      answers[0].innerHTML = questions[i].correct_answer // fino a riga 141 inserisco testo risposte in ogni contenitore con classe answer
+      answers[1].innerHTML = questions[i].incorrect_answers[0]
+      answers[2].innerHTML = questions[i].incorrect_answers[1]
+      answers[3].innerHTML = questions[i].incorrect_answers[2]
+      const countQuest = document.getElementById("countQuestions") 
+      countQuest.innerHTML = i+1 // nel paragrafo a piepagina inserisco l'indice dell'array di oggetti tiene il conto di ogni domanda 
+      for (let i1=0; i < answers.length; i1++){  // ad ogni bottone assegno la classe di selezione e la rimuovo a gli altri elementi 
+          answers[i1].addEventListener("click", function(e){
+              deselezionaClasse()
+              answers[i1].classList.add("select");
+          })
+        }              
+  }
+
+  //funzione timer countdown 60 secondi
+  const timer = function(){
+    let time 
+    let count = 60
+    let containerTimer = document.getElementById("timer")
+    containerTimer.innerHTML = count
+    time = setTimeout(update,1000)
+    function update(){
+        if(count>0){            
+            containerTimer.innerHTML = --count;
+          time = setTimeout(update,1000)
+            showQuestion(index)
+
+        }else{
+           // quando terminano i 60 secondi
+          answerUser = document.querySelector(".select");
+           //prendo la risposta selezionata dall'utente
+          if(answerUser){ // verifico che la classe sia stata assegnata almeno ad una risposta
+            checkAnswer() //true verifico se la risposta è corretta con la funzione apposita passando come parametro la risposta con la classe selezione 
+          }else{
+            contIncorrect +=1 //se non ha selezionato nessuna risposta incremento in automatico il contatore delle risposte errate             
+          } 
+          clearInterval(count);
+          index++;
+        } // quando terminano i 60 secondi incremento l'indice per mostrare la domanda successiva
+    }
+}
+timer()
+
+ 
+  const checkAnswer = () =>{
+    let correctAns = questions[i].correct_answer
+    if(answerUser == correctAns){
+      contCorrect += 1;
+    }
+    else{
+      contIncorrect += 1;
     }
   }
- 
-/*   timer()  *//* ho richiamato la funzione timer per controllare il suo andamento sulla pagina*/
-showQuestion() //richiamo la funzione per controllare il suo andamento
+
+// FINE PARTE RAMONA
+ /* 
+ //funzione timer countdown 60 secondi
+  const timer = function(indexCount){ /* aggiunta di un argomento nella funzione timer per poter risettare facilmente il count */
+  let time 
+  let end
+  let count = indexCount
+  let containerTimer = document.querySelector('#timer span')
+  containerTimer.innerHTML = count
+  time = setTimeout(update,1000)
+  function update(){
+      if(count>0){
+          containerTimer.innerHTML = --count
+          time = setTimeout(update,1000)            
+      }else{
+          end = "fine"
+      }
+  }
+  return count /* importante aggiungere il return per tracciare il count */
+}
+
+//funzione che deseleziona 
+const deselezionaClasse = function (){
+  const selezionati = document.querySelector(".isSelected");
+  if (selezionati){
+      selezionati.classList.remove("isSelected");
+  } 
+} 
+
+//funzione crea domande 
+const showQuestion = function(){
+  
+  for(let i=0; i < questions.length; i++){ // ogni domanda
+      let count = timer(60)
+      const quest = document.getElementById("quest")  
+      const answer = document.getElementsByClassName("answer")
+      quest.innerHTML = questions[i].question; //inserisco testo domanda
+      answer[0].innerHTML = questions[i].correct_answer // fino a riga 141 inserisco testo risposte in ogni contenitore con classe answer
+      answer[1].innerHTML = questions[i].incorrect_answers[0]
+      answer[2].innerHTML = questions[i].incorrect_answers[1]
+      answer[3].innerHTML = questions[i].incorrect_answers[2]
+      const countQuest = document.querySelector('#countQuestion span') 
+      countQuest.innerHTML = i+1 // nel paragrafo a piepagina inserisco l'indice dell'array di oggetti tiene il conto di ogni domanda 
+      for (let i=0; i < answer.length; i++){  // ad ogni bottone assegno la classe di selezione e la rimuovo a gli altri elementi 
+          answer.addEventListener("click", function(e){
+              deselezionaClasse()
+              answer.classList.add("isSelected");
+          })
+      }
+      if(count>0){ 
+          //l'utente ha ancora tempo di rispondere
+      }
+      else{
+          //passiamo alla prossima domanda
+      }               
+  }
+}
